36cad583b7ea390612f405de3018380827dfeda8
cristianames <cristianames.92@gmail.com>
2014-05-10 19:16:56 -0300

    Actualizada a la base de datos principal

--- SQL/scripts/Creacion.sql
+++ SQL/scripts/Creacion.sql
@@ -1,4 +1,4 @@
-create table pruebasDeScript.dbo.TG_Usuario(
+create table GD1C2014.dbo.TG_Usuario(
 	ID_User numeric(18,0) identity(1000,1) Primary key,
 	Username varchar(80) unique,
 	Pass varchar(20),
@@ -6,19 +6,19 @@ create table pruebasDeScript.dbo.TG_Usuario(
 	Antiguo bit
 )
 
-create table pruebasDeScript.dbo.TG_Rol(
+create table GD1C2014.dbo.TG_Rol(
 	ID_Rol numeric(18,0) identity(1,1) primary key,
 	Nombre varchar(20) unique,
 	Inhabilitado bit
 )
 
-create table pruebasDeScript.dbo.TG_Rubro(
+create table GD1C2014.dbo.TG_Rubro(
 	ID_Rubro numeric(18,0) identity(1,1) primary key,
 	Nombre varchar(20) unique,
 	Descripcion varchar(50)
 )
 
-create table pruebasDeScript.dbo.TG_Visibilidad(
+create table GD1C2014.dbo.TG_Visibilidad(
 	ID_Visibilidad numeric(18,0) identity(1,1) primary key,
 	Nombre varchar(20) unique,
 	Descripcion varchar(50),
@@ -26,31 +26,31 @@ create table pruebasDeScript.dbo.TG_Visibilidad(
 	Porcentaje_Venta numeric(18,0)
 )
 
-create table pruebasDeScript.dbo.TG_Factura(
+create table GD1C2014.dbo.TG_Factura(
 	ID_Factura numeric(18,0) identity(100000,1) primary key,
 	Fecha date,
 	Forma_Pago varchar(30),
 	Nro_Tarjeta numeric(20,0)
 )
-create table pruebasDeScript.dbo.TG_Roles_x_Usuario(
+create table GD1C2014.dbo.TG_Roles_x_Usuario(
 	ID_User numeric(18,0) references TG_Usuario(ID_User),
 	ID_Rol numeric(18,0) references TG_Rol(ID_Rol),
 	Inhabilitado bit,
 	Primary Key(ID_User,ID_Rol)
 )
 
-create table pruebasDeScript.dbo.TG_Funcionalidades_x_Rol(
+create table GD1C2014.dbo.TG_Funcionalidades_x_Rol(
 	ID_Rol numeric(18,0) references TG_Rol(ID_Rol) Primary Key,
 	Nombre varchar(20),
 	Descripcion varchar(50)
 )
 
-create table pruebasDeScript.dbo.TG_Caracteristicas_x_Rol(
+create table GD1C2014.dbo.TG_Caracteristicas_x_Rol(
 	ID_Rol numeric(18,0) references TG_Rol(ID_Rol) Primary Key,
 	Caracteristicas varchar(50)
 )
 
-create table pruebasDeScript.dbo.TG_Informacion(
+create table GD1C2014.dbo.TG_Informacion(
 	ID_User numeric(18,0),
 	ID_Rol numeric(18,0),
 	Razon_Social varchar(30),
@@ -66,7 +66,7 @@ create table pruebasDeScript.dbo.TG_Informacion(
 	Foreign Key(ID_User,ID_Rol) references TG_Roles_x_Usuario(ID_User,ID_Rol)
 )
 
-create table pruebasDeScript.dbo.TG_Direccion(
+create table GD1C2014.dbo.TG_Direccion(
 	ID_User numeric(18,0),
 	ID_Rol numeric(18,0),
 	Dire_Calle varchar(50),
@@ -79,7 +79,7 @@ create table pruebasDeScript.dbo.TG_Direccion(
 	Foreign Key(ID_User,ID_Rol) references TG_Roles_x_Usuario(ID_User,ID_Rol)
 )
 
-create table pruebasDeScript.dbo.TG_Publicacion(
+create table GD1C2014.dbo.TG_Publicacion(
 	ID_Publicacion numeric(18,0) identity(1000000,1) Primary Key,
 	Descripcion varchar(50),
 	Stock numeric(18,0),
@@ -95,7 +95,7 @@ create table pruebasDeScript.dbo.TG_Publicacion(
 	Foreign Key(ID_Vendedor,ID_Rol) references TG_Roles_x_Usuario(ID_User,ID_Rol)	
 )
 
-create table pruebasDeScript.dbo.TG_Oferta(
+create table GD1C2014.dbo.TG_Oferta(
 	ID_Oferta numeric(18,0) identity(1000000,1) Primary Key,
 	ID_Ofertante numeric(18,0) references TG_Usuario(ID_User),
 	ID_Publicacion numeric(18,0) references TG_Publicacion(ID_Publicacion),
@@ -104,7 +104,7 @@ create table pruebasDeScript.dbo.TG_Oferta(
 	Monto_Oferta numeric(18,0),
 )
 
-create table pruebasDeScript.dbo.TG_Compra(
+create table GD1C2014.dbo.TG_Compra(
 	ID_Operacion numeric(18,0) identity(1000000,1) Primary Key,
 	ID_Comprador numeric(18,0) references TG_Usuario(ID_User),
 	ID_Factura numeric(18,0) references TG_Factura(ID_Factura),
@@ -117,13 +117,13 @@ create table pruebasDeScript.dbo.TG_Compra(
 	Detalle varchar(50)
 )
 
-create table pruebasDeScript.dbo.TG_Publicaciones_x_Rubro(
+create table GD1C2014.dbo.TG_Publicaciones_x_Rubro(
 	ID_Rubro numeric(18,0) references TG_Rubro(ID_Rubro),
 	ID_Publicacion numeric(18,0) references TG_Publicacion(ID_Publicacion),
 	Primary Key(ID_Rubro,ID_Publicacion)
 )
 
-create table pruebasDeScript.dbo.TG_Preguntas(
+create table GD1C2014.dbo.TG_Preguntas(
 	ID_Pregunta numeric(18,0) identity(1000000,1) Primary Key,
 	ID_Publicacion numeric(18,0) references TG_Publicacion(ID_Publicacion),
 	Pregunta varchar(256),
