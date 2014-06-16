a91f3d70308a6ba719b418cba2409f9f0381b4f2
Dante Castelluccio <dantecastelluccio@gmail.com>
2014-06-15 22:09:34 -0300

    Revert "bajando program again"
    This reverts commit d837996dcb729ca4ecc6c6592c5d86c0454763d2.

---
+++ CSharp/TP Frba Commerce 1.0 - 1C 2014/Aplicacion Desktop/FrbaCommerce/FrbaCommerce/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace FrbaCommerce
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Portal());
+        }
+
+    }
+}
